18ca477fe10a46dce41687447b55862ed3cf2c0d
naginsulka <sulkanagin@gmail.com>
2024-04-23 20:14:33 +0530

    h

--- app/calculator/components/CalculatorBody.js
+++ app/calculator/components/CalculatorBody.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const Button = ({ value }) => (
-  <div className="w-1/4 h-16 flex justify-center items-center bg-gray-800 text-lg font-semibold cursor-pointerr">
+  <div className="w-1/4 h-16 flex justify-center items-center bg-gray-800 text-lg font-semibold">
     {value}
   </div>
 );
 
 const CalculatorBody = () => {
-  return (<div className='flex justify-center'>
+  return (
+  <div className='flex justify-center'>
     <div className='flex flex-col justify-evenly bg-gray-400 w-[20%] h-[65vh]'>
 
       <div className='mx-auto'><input type="text" className='bg-gray-600' /></div>
@@ -17,7 +18,7 @@ const CalculatorBody = () => {
         <Button value="DE" />
         <Button value="." />
         <Button value="/" />
-      </div>  
+      </div>
 
       <div className='flex space-x-3 px-3'>
         <Button value="7" />

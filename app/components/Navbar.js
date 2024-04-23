e085435bd7942ee9992f9d954f8fcdacf3715fe8
naginsulka <sulkanagin@gmail.com>
2024-04-24 02:33:22 +0530

    g

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = (props) => {
   return (
-    <nav className="bg-gray-800 text-white shadow-lg border-4">
+    <nav className="bg-gray-800 text-white shadow-lg border-4 mb-24">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
@@ -12,7 +12,7 @@ const Navbar = (props) => {
             <div className="hidden md:block">
               <ul className="flex space-x-8">
                 <a className="px-3 py-2 rounded-md text-lg font-medium hover:bg-gray-700 hover:text-white" href="/calculator">Calculator</a>
-                <a className="px-3 py-2 rounded-md text-lg font-medium hover:bg-gray-700 hover:text-white" href="/services">Services</a>
+                <a className="px-3 py-2 rounded-md text-lg font-medium hover:bg-gray-700 hover:text-white" href="/todolist">TodoList</a>
                 <a className="px-3 py-2 rounded-md text-lg font-medium hover:bg-gray-700 hover:text-white" href="/work">Our Work</a>
               </ul>
             </div>
